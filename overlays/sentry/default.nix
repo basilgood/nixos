e9f1e5b5ee033b3d18f1f43af2f394553d8ba52e
vasile luta <elsile69@yahoo.com>
2019-09-26 11:31:47 +0300

    sentry: final

--- overlays/sentry/default.nix
+++ overlays/sentry/default.nix
@@ -1,5 +1,17 @@
 self: super:
-#let python = import ./requirements.nix { pkgs = super; };
-#in { sentry = python.packages.sentry; }
-{}
-
+let
+  pkgs = import (builtins.fetchTarball {
+    url = "https://github.com/NixOS/nixpkgs/archive/4ca0df15390.tar.gz";
+    sha256 = "1ap9zchhm1vd8fpdfigigxx8qq5mk3nmpwddqjyzba7g4hm04i9y";
+  }) { };
+  sentryPython = pkgs.python27.override {
+    packageOverrides = (python-self: python-super:
+      {
+        semaphore = python-super.callPackage ./semaphore.nix { };
+        symbolic = python-super.callPackage ./symbolic.nix { };
+        pkg-config = super.pkgconfig;
+      } // (import ./overrides.nix) python-self python-super {
+        inherit (pkgs) postgresql;
+      });
+  };
+in { sentry = sentryPython.pkgs.callPackage ./sentry.nix { }; }
---
+++ overlays/sentry/semaphore.nix
@@ -0,0 +1,45 @@
+{ rustPlatform, fetchFromGitHub, fetchPypi, pkg-config, cmake, openssl
+, buildPythonPackage, milksnake, setuptools_scm }:
+
+buildPythonPackage rec {
+  pname = "semaphore";
+  version = "0.4.42";
+  name = "${pname}-${version}";
+  src = fetchPypi {
+    inherit pname version;
+    extension = "zip";
+    sha256 = "0z7z1j6qn2k14lyqkdrwvvv531zyfdc0zc3g7b2d20mjgcvsxgb0";
+  };
+
+  native = rustPlatform.buildRustPackage {
+    name = "${name}-native";
+    src = fetchFromGitHub {
+      owner = "getsentry";
+      repo = pname;
+      rev = "${version}";
+      sha256 = "1l903q5w6p6jz9xy8120fdv67479vvrzxx5ssnnqiwmd6lmb7ps6";
+      fetchSubmodules = true;
+    };
+    sourceRoot = "source/cabi";
+    cargoSha256 = "0723h8qcbg6yqsmxw4wmbdvq5jf70h8g0xminar06p0hrz4l3sx3";
+    nativeBuildInputs = [ pkg-config ];
+    buildInputs = [ openssl ];
+    postInstall = ''
+      mkdir -p $out/include
+      cp include/semaphore.h $out/include/
+    '';
+    doCheck = false;
+  };
+
+  propagatedBuildInputs = [ milksnake setuptools_scm ];
+
+  patches = [ ./semaphore.patch ];
+
+  preBuild = ''
+    mkdir -p rust/target/release rust/include
+    cp ${native}/lib/libsemaphore.so rust/target/release/libsemaphore.so
+    cp ${native}/include/semaphore.h rust/include/semaphore.h
+  '';
+
+  doCheck = false;
+}
