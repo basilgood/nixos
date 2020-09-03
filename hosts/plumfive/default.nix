2106cac6900f0ed166e48f98da08e20e80a39059
vasile luta <elsile69@yahoo.com>
2020-09-03 10:11:13 +0300

    feat(hosts): simple swaybar

--- hosts/plumfive/default.nix
+++ hosts/plumfive/default.nix
@@ -8,47 +8,28 @@
 
   networking.hostName = "plumfive";
   programs.sway = {
-    status = with pkgs; "${i3status-rust}/bin/i3status-rs ${
-        writeText "status.toml" ''
-          [theme]
-          name = "slick"
-          [theme.overrides]
-          idle_bg = "#2E3440"
-          idle_fg = "#839496"
-          separator = ""
-          [icons]
-          name = "awesome"
-          [[block]]
-          block = "memory"
-          display_type = "memory"
-          format_mem = "{Mug}G"
-          format_swap = "{SUp}%"
-          [[block]]
-          block = "cpu"
-          format = "{utilization}% {frequency}"
-          [[block]]
-          block = "temperature"
-          collapsed = false
-          interval = 1
-          format = "{max}°"
-          chip= "k10temp-*"
-          idle= 70
-          info= 75
-          warning= 80
-          [[block]]
-          block = "temperature"
-          collapsed = false
-          interval = 1
-          format = "{max}°"
-          chip= "amdgpu-*"
-          [[block]]
-          block = "sound"
-          [[block]]
-          block = "time"
-          interval = 60
-          format = "%a %d/%m %R"
-        ''
-      }";
+    status =
+      let
+        bar = pkgs.writeShellScriptBin "script.sh" ''
+          date_formatted=" "$(date "+%a %d/%m %R")
+          batcapacity=$(cat /sys/class/power_supply/BAT1/capacity )%
+          battery=$(cat /sys/class/power_supply/BAT1/status | head -c 1 | awk '''{print tolower($0)}''') $batcapacity
+          [ ! -z $battery ] && ([ $batcapacity -lt 10 ] && \\e[31m"  "$battery || \\e[39m"  "$battery)
+          audio_volume=$(${pkgs.pamixer}/bin/pamixer --get-volume)
+          audio_is_muted=$(${pkgs.pamixer}/bin/pamixer --get-mute)
+          [ $audio_is_muted = "true" ] && audio="婢 mute" || audio=" "$audio_volume%
+          mem=" "$(free -m | ${pkgs.gawk}/bin/awk 'NR==2{printf "%.1fG", $3/1024; exit}')
+          cpu="  "$(${pkgs.gawk}/bin/awk '{ print $1; exit }' /proc/loadavg)
+          cpuf=$((`cat /sys/devices/system/cpu/cpu0/cpufreq/scaling_cur_freq` / 1000))
+          cput=" "$(${pkgs.lm_sensors}/bin/sensors k10temp-pci-* | ${pkgs.gawk}/bin/awk '/Tdie/ {print $2}' | cut -c 2-5)
+          gput=$(${pkgs.lm_sensors}/bin/sensors amdgpu-pci-* | ${pkgs.gawk}/bin/awk '/edge/ {print $2}' | cut -c 2-5)
+          echo $mem $cpu $cpuf $cput $gput $audio $battery_pluggedin $battery_time $battery_charge $date_formatted
+        '';
+      in
+      ''
+        while ${bar}/bin/script.sh; do sleep 1; done
+      '';
+
     extraConfig = ''
       output * bg ${./wall.jpg} fill
         for_window {
