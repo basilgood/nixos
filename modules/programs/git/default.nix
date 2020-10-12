0906d527d1ce30acaf84852578f5d5c92cb8daa7
basilgood <elsile69@yahoo.com>
2020-10-12 10:15:55 +0300

    chore(git module): updates

--- modules/programs/git/default.nix
+++ modules/programs/git/default.nix
@@ -50,7 +50,22 @@ in {
         default = "${pkgs.less}/bin/less";
       };
 
-      extraConfig = mkOption {
+      status = mkOption {
+        type = types.lines;
+        default = ''
+          showuntrackedfiles = all
+        '';
+      };
+
+      rerere = mkOption {
+        type = types.lines;
+        default = ''
+          enabled = 1
+          autoupdate = 1
+        '';
+      };
+
+      diff = mkOption {
         type = types.lines;
         default = "";
       };
@@ -65,76 +80,78 @@ in {
         default = "nvim";
       };
 
-      interface = mkOption {
-        type = types.package;
-        default = pkgs.gitAndTools.tig;
+      extraConfig = mkOption {
+        type = types.lines;
+        default = "";
+      };
+
+      extraPackages = mkOption {
+        type = types.listOf types.package;
+        default = [];
       };
     };
   };
 
-  config = mkIf cfg.enable (mkMerge [
-    {
-      environment = {
-        systemPackages = with pkgs; [ git cfg.interface ];
-        etc."gitconfig".text = ''
-          [user]
-            name = ${cfg.name}
-            email = ${cfg.email}
-          [core]
-            editor = ${cfg.editor}
-            pager = ${cfg.pager}
-            excludesfile = ~/.gitignore
-          [status]
-            showuntrackedfiles = all
-          [rerere]
-            enabled = 1
-            autoupdate = 1
-          [color]
-            diff = auto
-            status = auto
-            branch = auto
-            ui = auto
-          [color "diff"]
-            meta = blue
-            frag = black
-            old = red
-            new = green
-          [color "status"]
-            added = green
-            changed = yellow
-            untracked = cyan
-          [color "branch"]
-            current = yellow reverse
-            local = yellow
-            remote = green
-          [alias]
-            lg = log --oneline --graph --all
-          ${cfg.extraConfig}
-          [diff]
-            prompt = false
-            tool = diff_tool
-          [difftool "diff_tool"]
-            cmd = ${difftools.${cfg.difftool}}
-          [merge]
-            tool = diff_tool
-            conflictstyle = diff3
-          [mergetool "diff_tool"]
-            prompt = false
-            keepBackup = false
-            cmd = ${mergetools.${cfg.mergetool}}
-        '';
-      };
-    }
+  config = mkIf cfg.enable (
+    mkMerge [
+      {
+        environment = {
+          systemPackages = with pkgs; [ git ] ++ cfg.extraPackages;
+          etc."gitconfig".text = ''
+            [user]
+              name = ${cfg.name}
+              email = ${cfg.email}
+            [core]
+              editor = ${cfg.editor}
+              pager = ${cfg.pager}
+              excludesfile = ~/.gitignore
+            [status]
+              showuntrackedfiles = all
+            [rerere]
+              enabled = 1
+              autoupdate = 1
+            [color "diff"]
+              meta = blue
+              frag = black
+              old = red
+              new = green
+            [color "status"]
+              added = green
+              changed = yellow
+              untracked = cyan
+            [color "branch"]
+              current = yellow reverse
+              local = yellow
+              remote = green
+            [diff]
+              prompt = false
+              tool = diff_tool
+            [difftool "diff_tool"]
+              cmd = ${difftools.${cfg.difftool}}
+            [merge]
+              tool = diff_tool
+              conflictstyle = diff3
+            [mergetool "diff_tool"]
+              prompt = false
+              keepBackup = false
+              cmd = ${mergetools.${cfg.mergetool}}
+            ${cfg.extraConfig}
+          '';
+        };
+      }
 
-    (mkIf cfg.lfsEnable {
-      environment.systemPackages = [ pkgs.git-lfs ];
-      environment.etc."gitconfig".text = mkAfter ''
-        [filter "lfs"]
-          clean = git-lfs clean -- %f
-          smudge = git-lfs smudge -- %f
-          process = git-lfs filter-process
-          required = true
-      '';
-    })
-  ]);
+      (
+        mkIf cfg.lfsEnable {
+          environment.systemPackages = [ pkgs.git-lfs ];
+          environment.etc."gitconfig".text = mkAfter ''
+            [filter "lfs"]
+              clean = git-lfs clean -- %f
+              smudge = git-lfs smudge -- %f
+              process = git-lfs filter-process
+              required = true
+          '';
+        }
+      )
+    ]
+  );
 }
